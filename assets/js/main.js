4952c422201d714c3c0e59839f2f62f24f3991c1
apla77 <verocidade77@gmail.com>
2021-01-24 20:18:03 -0300

    Validação dos campos no form

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,30 @@ class ValidaForm{
 
     handleSubmit(e){
         e.preventDefault();
-        console.log('Formulario não será enviado...');
+        const camposvalidos = this.validaCampos();
+    }
+
+    validaCampos(){
+        let valida = true;
+
+        for(let erroText of this.formulario.querySelectorAll('.erro-text')){
+            erroText.remove();
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            if(!campo.value){
+                const label = campo.previousElementSibling.innerText;
+                this.exibeErro(campo, `O campo "${label}" não pode estar em branco!.`);
+                valida = false;
+            }
+        }
+    }
+
+    exibeErro(campo, msg){
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('erro-text');
+        campo.insertAdjacentElement('afterend', div);
     }
 }
 
